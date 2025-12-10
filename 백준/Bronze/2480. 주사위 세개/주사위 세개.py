37e19a3ea5407dde640005958dbeb43682a28781
ddunddan <100018615+ddunddan@users.noreply.github.com>
2025-12-10 13:10:21 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,16 @@
+a, b, c = map(int, input().split())
+
+
+if a == b and b == c:
+    money = 10000 + (a * 1000)
+elif a == b or a == c or b == c:
+    if a == b:
+        money = 1000 + (a*100)
+    elif a == c:
+        money = 1000 + (a*100)
+    else:
+        money = 1000 + (b*100)
+else:
+    money = max(a,b,c)*100
+    
+print(money)
